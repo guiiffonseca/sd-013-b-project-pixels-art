a19c23684d845cce213c85028694828213e0a984
guiiffonseca <guii.ffonseca22@gmail.com>
2021-06-08 15:18:37 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const board = document.querySelectorAll('.pixel');
 
 //Requisito 6 e 7
 for (let index = 0; index < selectedColor.length; index += 1) {
-    selectedColor[index].addEventListener ('click', selector)}
+    selectedColor[index].addEventListener ('click', selector)
+}
 function selector (event) {
     document.querySelector('.selected').classList.remove('selected')
     event.target.classList.add('selected');
@@ -12,7 +13,7 @@ function selector (event) {
 
 //Requisito 8
 for (let index = 0; index < board.length; index += 1){
-    board[index].addEventListener('click', paintingBoard)
+    board[index].addEventListener('click', paintingBoard);
 }
 function paintingBoard (event) {
         let color = document.querySelector('.selected');
@@ -20,6 +21,15 @@ function paintingBoard (event) {
         event.target.style.backgroundColor = colorBoard;
 }
 
+//Requisito 9
+const eraser = document.querySelector('#clear-board');
+eraser.addEventListener('click', clear);
+
+function clear (event) {
+    for (let index = 0; index < board.length; index += 1){
+        board[index].style.backgroundColor = ' white'
+    }
+}
 
 
 
