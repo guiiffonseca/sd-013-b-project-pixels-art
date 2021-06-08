939acad77b568051b5ed2f55f6d32c7f9baa32c2
guiiffonseca <guii.ffonseca22@gmail.com>
2021-06-08 14:53:03 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 
 const selectedColor = document.getElementsByClassName('color');
-console.log(selectedColor);
+const board = document.querySelectorAll('.pixel');
 
-
-for (let index = 0; index < selectedColor.length; index+=1) {
+//Requisito 6 e 7
+for (let index = 0; index < selectedColor.length; index += 1) {
     selectedColor[index].addEventListener ('click', selector)}
-    
 function selector (event) {
-   
     document.querySelector('.selected').classList.remove('selected')
     event.target.classList.add('selected');
 }
+
+//Requisito 8
+for (let index = 0; index < board.length; index += 1){
+    board[index].addEventListener('click', paintingBoard)
+}
+function paintingBoard (event) {
+        let color = document.querySelector('.selected');
+        let colorBoard = getComputedStyle(color).backgroundColor;
+        event.target.style.backgroundColor = colorBoard;
+}
+
+
+
+
+
+
+
+
+
+
